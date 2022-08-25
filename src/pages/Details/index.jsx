6ac7cf48b0e329e395de4e6405805fa729cc5f32
Rafael Barros de Almeida e Silva <rafa_barros92@hotmail.com>
2022-08-24 21:01:16 -0300

    [Stage09ExercicioAula] Add new components

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,9 +1,12 @@
+import { Button } from '../../components/Button/index.jsx'
 import { Container } from './styles.js'
 
 export function Details() {
     return (
         <Container>
             <h1>Oláa</h1>
+
+            <Button title="Voltar"/>
         </Container>
     )
 }
