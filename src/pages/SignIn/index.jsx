7788c1e8c631f2c1eba6fd02b0a6c0342f2750e4
Rafael Barros de Almeida e Silva <rafa_barros92@hotmail.com>
2022-09-01 19:54:06 -0300

    [Stage09ExercicioAula] Add SignIn page

---
+++ src/pages/SignIn/index.jsx
@@ -0,0 +1,37 @@
+import { Background, Container, Form } from "./styles";
+import { FiMail, FiLock } from "react-icons/fi";
+
+
+import { Input } from "../../components/Input";
+import { Button } from "../../components/Button";
+
+export function SignIn() {
+    return (
+        <Container>
+            <Form>
+                <h1>Rocket Notes</h1>
+                <p>Aplicação para salvar e gerenciar seus links úteis</p>
+
+                <h2>Faça seu login</h2>
+
+                <Input
+                    type="text"
+                    placeholder="E-mail"
+                    icon={FiMail}
+                />
+                <Input 
+                    type="password"
+                    placeholder="Senha"
+                    icon={FiLock}
+                />
+
+                <Button title="Entrar"></Button>
+
+
+                <a href="#">Criar conta</a>
+            </Form>
+
+            <Background />
+        </Container>
+    )
+}
