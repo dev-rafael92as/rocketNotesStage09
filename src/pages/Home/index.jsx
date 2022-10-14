b59a96a8c511b5e993b3556abdf090e4ad799ff9
Rafael Barros de Almeida e Silva <rafa_barros92@hotmail.com>
2022-10-13 22:37:10 -0300

    [feature] Add filter logic on Home page

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,14 +1,39 @@
 import { Header } from "../../components/Header";
 import { ButtonText } from "../../components/ButtonText";
-import { Link } from "react-router-dom";
+
 
 import { FiPlus, FiSearch } from "react-icons/fi"
 import { Container, Brand, Menu, Search, Content, NewNote } from "./styles";
 import { Input } from "../../components/Input";
 import { Note } from "../../components/Note";
 import { Section } from "../../components/Section";
+import { useEffect, useState } from "react";
+import { api } from "../../services/api";
 
 export function Home() {
+    const [ tags, setTags ] = useState([]);
+    const [ tagsSelected, setTagsSelected ] = useState([]);
+
+    function handleTagSelected(tagName) {
+        const alreadySelected = tagsSelected.includes(tagName);
+        
+        if (alreadySelected) {
+            const filteredTags = tagsSelected.filter(tag => tag !== tagName);
+            setTagsSelected(filteredTags)
+        } else {
+            setTagsSelected(prevState => [...prevState, tagName]);
+        }
+    }
+
+    useEffect(() => {
+        async function fetchTags() {
+            const response = await api.get("/tags");
+            setTags(response.data)
+        }
+        fetchTags()
+    },[])
+
+
     return (
         <Container>
             <Brand>
@@ -19,17 +44,23 @@ export function Home() {
 
             <Menu>
                 <li>
-                    <ButtonText title="Todos" isActive></ButtonText>
+                    <ButtonText 
+                        title="Todos" 
+                        onClick={() => handleTagSelected("all")}
+                        isActive={tagsSelected.length === 0}
+                    ></ButtonText>
                 </li>
-                <li>
-                    <ButtonText title="Frontend"></ButtonText>
-                </li>
-                <li>
-                    <ButtonText title="Node"></ButtonText>
-                </li>
-                <li>
-                    <ButtonText title="React"></ButtonText>
-                </li>
+                {
+                    tags && tags.map(tag => (
+                        <li key={String(tag.id)}>
+                            <ButtonText 
+                                title={tag.name}
+                                onClick={() => handleTagSelected(tag.name)}
+                                isActive={tagsSelected.includes(tag.name)}
+                            ></ButtonText>
+                        </li>
+                    ))
+                }
             </Menu>
 
             <Search>
