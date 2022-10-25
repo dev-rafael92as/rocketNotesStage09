68b0ec800c0a3290903a9f984fb2b9de6dcfed1a
Rafael Barros de Almeida e Silva <rafa_barros92@hotmail.com>
2022-10-24 23:01:26 -0300

    [feature] Add new feat

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,10 +2,17 @@ import { Container, Profile, Logout } from './styles';
 import { RiShutDownLine } from 'react-icons/ri';
 import { useAuth } from '../../hooks/auth';
 import { api } from '../../services/api';
+import { useNavigate } from 'react-router-dom';
 
 
 export function Header() {
     const { signOut, user } = useAuth()
+    const natigation = useNavigate()
+
+    function handleSignOut() {
+        natigation("/")
+        signOut()
+    }
 
     const avatarUrl = user.avatar ? `${api.defaults.baseURL}/files/${user.avatar}` : avatarPlaceholder
 
@@ -21,7 +28,7 @@ export function Header() {
                 </div>
             </ Profile>
 
-            <Logout onClick={ signOut }>
+            <Logout onClick={ handleSignOut }>
                 <RiShutDownLine />
             </Logout>
 		</ Container>
